495f7d210a979338481edd9aeb5010036a1e39c7
dimokfl <dimokfl@mail.ru>
2021-05-28 19:54:33 +0400

    Homework 2 is done

--- core/src/ru/stargame/screen/MenuScreen.java
+++ core/src/ru/stargame/screen/MenuScreen.java
@@ -9,10 +9,14 @@ import ru.stargame.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final float LEN = 2f;
+
     private Texture img;
     private Texture background;
     private Vector2 pos;
     private Vector2 v;
+    private Vector2 dist;
+    private Vector2 tmp;
 
     @Override
     public void show() {
@@ -20,18 +24,26 @@ public class MenuScreen extends BaseScreen {
         img = new Texture("badlogic.jpg");
         background = new Texture("background.jpg");
         pos = new Vector2();
-        v = new Vector2(2, 1);
+        v = new Vector2();
+        dist = new Vector2();
+        tmp = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        pos.add(v);
         ScreenUtils.clear(1, 0, 0, 1);
         batch.begin();
         batch.draw(background, 0, 0, 1024, 512);
         batch.draw(img, pos.x, pos.y);
         batch.end();
+        tmp.set(dist);
+        if (tmp.sub(pos).len() <= v.len()){
+            pos.set(dist);
+            v.setZero();
+        } else {
+            pos.add(v);
+        }
     }
 
     @Override
@@ -43,7 +55,8 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
-        return super.touchDown(screenX, screenY, pointer, button);
+        dist.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(dist.cpy().sub(pos)).setLength(LEN);
+        return false;
     }
 }
