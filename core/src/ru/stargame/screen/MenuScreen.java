9c68badf2cb269e03c8a4660cedeaf17e5d1f03d
dimokfl <dimokfl@mail.ru>
2021-06-01 23:02:17 +0400

    HomeWork 3 done

--- core/src/ru/stargame/screen/MenuScreen.java
+++ core/src/ru/stargame/screen/MenuScreen.java
@@ -7,12 +7,15 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import ru.stargame.base.BaseScreen;
 import ru.stargame.math.Rect;
 import ru.stargame.sprite.Background;
+import ru.stargame.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
+    private Texture badlogic;
 
     private Background background;
+    private Logo logo;
 
 
     @Override
@@ -20,20 +23,25 @@ public class MenuScreen extends BaseScreen {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        badlogic = new Texture("badlogic.jpg");
+        logo = new Logo(badlogic);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        logo.update(delta);
         ScreenUtils.clear(0.3f, 0.5f, 0.4f, 1);
         batch.begin();
         background.draw(batch);
+        logo.draw(batch);
         batch.end();
     }
 
@@ -41,10 +49,12 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         bg.dispose();
+        badlogic.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch, pointer, button);
         return false;
     }
 }
